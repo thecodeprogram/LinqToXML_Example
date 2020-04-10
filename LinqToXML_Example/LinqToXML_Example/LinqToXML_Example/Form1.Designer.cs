f3269f4de741d364eea437c863b9471a058b6370
Burak Hamdi TUFAN <43945916+thecodeprogram@users.noreply.github.com>
2020-04-10 19:02:01 +0300

    Add files via upload

---
+++ LinqToXML_Example/LinqToXML_Example/LinqToXML_Example/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace LinqToXML_Example
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtRole = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSalary = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblTotalEmployee = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lstEployees = new System.Windows.Forms.ListView();
+            this.ID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.E_Name = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Surname = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Role = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Salary = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(75, 38);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Location = new System.Drawing.Point(75, 64);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(100, 20);
+            this.txtSurname.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Surname";
+            // 
+            // txtRole
+            // 
+            this.txtRole.Location = new System.Drawing.Point(75, 90);
+            this.txtRole.Name = "txtRole";
+            this.txtRole.Size = new System.Drawing.Size(100, 20);
+            this.txtRole.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Role";
+            // 
+            // txtSalary
+            // 
+            this.txtSalary.Location = new System.Drawing.Point(75, 116);
+            this.txtSalary.Name = "txtSalary";
+            this.txtSalary.Size = new System.Drawing.Size(100, 20);
+            this.txtSalary.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 119);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Salary";
+            // 
+            // lblTotalEmployee
+            // 
+            this.lblTotalEmployee.AutoSize = true;
+            this.lblTotalEmployee.Location = new System.Drawing.Point(182, 227);
+            this.lblTotalEmployee.Name = "lblTotalEmployee";
+            this.lblTotalEmployee.Size = new System.Drawing.Size(77, 13);
+            this.lblTotalEmployee.TabIndex = 9;
+            this.lblTotalEmployee.Text = "Total Count : 0";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(75, 143);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(100, 23);
+            this.btnAdd.TabIndex = 10;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(76, 172);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(100, 23);
+            this.btnUpdate.TabIndex = 11;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(76, 201);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(100, 23);
+            this.btnRemove.TabIndex = 12;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // txtID
+            // 
+            this.txtID.Enabled = false;
+            this.txtID.Location = new System.Drawing.Point(75, 12);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(100, 20);
+            this.txtID.TabIndex = 14;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(10, 15);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(18, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "ID";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(549, 12);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(100, 20);
+            this.txtSearch.TabIndex = 17;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(484, 15);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Search";
+            // 
+            // lstEployees
+            // 
+            this.lstEployees.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ID,
+            this.E_Name,
+            this.Surname,
+            this.Role,
+            this.Salary});
+            this.lstEployees.FullRowSelect = true;
+            this.lstEployees.Location = new System.Drawing.Point(185, 41);
+            this.lstEployees.MultiSelect = false;
+            this.lstEployees.Name = "lstEployees";
+            this.lstEployees.Size = new System.Drawing.Size(464, 183);
+            this.lstEployees.TabIndex = 18;
+            this.lstEployees.UseCompatibleStateImageBehavior = false;
+            this.lstEployees.View = System.Windows.Forms.View.Details;
+            this.lstEployees.Click += new System.EventHandler(this.lstEployees_Click);
+            // 
+            // ID
+            // 
+            this.ID.Text = "ID";
+            // 
+            // E_Name
+            // 
+            this.E_Name.Text = "Name";
+            this.E_Name.Width = 116;
+            // 
+            // Surname
+            // 
+            this.Surname.Text = "Surname";
+            this.Surname.Width = 115;
+            // 
+            // Role
+            // 
+            this.Role.Text = "Role";
+            this.Role.Width = 78;
+            // 
+            // Salary
+            // 
+            this.Salary.Text = "Salary";
+            this.Salary.Width = 70;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(661, 290);
+            this.Controls.Add(this.lstEployees);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lblTotalEmployee);
+            this.Controls.Add(this.txtSalary);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtRole);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtSurname);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Linq to XML example by THECODEPROGRAM";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtRole;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSalary;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblTotalEmployee;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ListView lstEployees;
+        private System.Windows.Forms.ColumnHeader E_Name;
+        private System.Windows.Forms.ColumnHeader Role;
+        private System.Windows.Forms.ColumnHeader Salary;
+        private System.Windows.Forms.ColumnHeader Surname;
+        private System.Windows.Forms.ColumnHeader ID;
+    }
+}
+
---
+++ LinqToXML_Example/LinqToXML_Example/LinqToXML_Example/Form1.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace LinqToXML_Example
+{
+    public partial class Form1 : Form
+    {
+
+        public const string xml_path = "employees.xml";
+        public const string root_Element = "Employees";
+        public const string second_element = "Employee";
+
+        public const string elementName_ID = "ID";
+        public const string elementName_Name = "Name";
+        public const string elementName_Surname = "Surname";
+        public const string elementName_Role = "Role";
+        public const string elementName_Salary = "Salary";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Load the employees at first to see who recorded.
+            loadEmployees();
+        }
+
+        //First I will write a function which will create the XML file if there is no XML file
+        public void createXMLFile()
+        {
+            //In this function we will use the XmlTextWriter to create an XML file.
+            XmlTextWriter xmlWriter = new XmlTextWriter(xml_path, UTF8Encoding.UTF8);
+            xmlWriter.WriteStartDocument();
+            //First, Here we will create it and we write the created date inside the file as a comment.
+            xmlWriter.WriteComment("XML File Created at first time at : " + DateTime.Now.ToString());
+            //We must write the root element of the XML firstly
+            //If we do not the program will not run and throw an exception 
+            //which specifies there is no root element. 
+            xmlWriter.WriteStartElement(root_Element);
+            //And we finis the writing the XML file
+            xmlWriter.WriteEndDocument();
+            xmlWriter.Close();
+        }
+
+        //We write a method to add a new element into the XML file under the main element with secon layer element.
+        public void addNewEmployee()
+        {
+            //To make anything with the XML file, first we have to check the file exist
+            //and if not exist, we must create the XML file with required root element
+            if (File.Exists(xml_path) == false)
+                createXMLFile();
+
+            //And then load the xml document.
+            XDocument document = XDocument.Load(xml_path);
+            if (document != null)
+            {
+                //After loading the element we must select the root element which we will work on.
+                XElement main_element = document.Element(root_Element);
+                //One of the most important thing of  a record is RECORD ID value.
+                //XML has no such thing Auto Identity, so we will count how many record exist in the XML
+                //and we will increase the element count for new record.
+                int employeeID;
+                try { employeeID = document.Descendants(second_element).Max(id => (int)id.Attribute(elementName_ID)); }
+                catch { employeeID = 0; }
+                employeeID++;
+                //And then we will create new Element under selected Root Element.
+                //This element layer will be second layer and all employee elements will be this element
+                //So we create all employee elements with this tag.
+                XElement element = new XElement(new XElement(second_element,
+                    new XElement(elementName_Name, txtName.Text),
+                    new XElement(elementName_Surname, txtSurname.Text),
+                    new XElement(elementName_Role, txtRole.Text),
+                    new XElement(elementName_Salary, txtSalary.Text)));
+                //We will set the element id as Attribute of the element.
+                element.SetAttributeValue(elementName_ID, employeeID);
+                //You can add comments above of all new elements with XComment.
+                element.AddFirst(new XComment("New Employee ID : " + employeeID));
+                //And add it under thhe main_element
+                main_element.Add(element);
+                //After all process do not forget to save the document.
+                document.Save(xml_path);
+
+                MessageBox.Show("Employee added successfully");
+
+                txtID.Text = "";
+                txtName.Text = "";
+                txtSurname.Text = "";
+                txtRole.Text = "";
+                txtSalary.Text = "";
+                //To show the user the updates, load the records again.
+                loadEmployees();
+            }
+        }
+
+        public void updateEmployee()
+        {
+            //First load the xml document.
+            XDocument document = XDocument.Load(xml_path);
+            if (document != null)
+            {
+                //First select the element with the specified ID value to update.
+                //Here we created an XElement from the document's root element. And then we select the second layer element
+                //with where condition we specified the selected second layer element must have a Attribute value that we specified in txtID
+                //and then select appropraite employee record in here.
+                XElement element = (from f in document.Element(root_Element).Elements(second_element)
+                                      where f.Attribute(elementName_ID).Value == txtID.Text
+                                      select f).SingleOrDefault();
+                //If selected element is not null, I mean there is a record with specified ID value
+                if (element != null)
+                {
+                    //Start to update elements' value from textboxes
+                    element.SetElementValue(elementName_Name, txtName.Text);
+                    element.SetElementValue(elementName_Surname, txtSurname.Text);
+                    element.SetElementValue(elementName_Role, txtRole.Text);
+                    element.SetElementValue(elementName_Salary, txtSalary.Text);
+                    //Last do not forget to save the XML file.
+                    document.Save(xml_path);
+
+                    MessageBox.Show("Employee updated successfully.");
+
+                    txtID.Text = "";
+                    txtName.Text = "";
+                    txtSurname.Text = "";
+                    txtRole.Text = "";
+                    txtSalary.Text = "";
+                    //Loading the entire records will be good.
+                    loadEmployees();
+
+                }
+            }
+        }
+
+        public void loadEmployees()
+        {
+            //First clear the listview to load records
+            lstEployees.Items.Clear();
+            //and we check the is exist
+            if (File.Exists(xml_path))
+            {
+                //if the file is exist load the XML document
+                XDocument document = XDocument.Load(xml_path);
+                if (document != null)
+                {
+                    //We load  all second layer elements with XElement type in an Enumerable under Root Element
+                    IEnumerable<XElement> employees = from f in document.Element(root_Element).Elements(second_element)
+                                                 select f;
+                    //If there is a record
+                    if (employees.Count() > 0)
+                    {
+                        //Load all records to the listview
+                        foreach (XElement emp in employees) 
+                        {
+                            ListViewItem item1 = new ListViewItem(emp.Attribute(elementName_ID).Value);
+                            item1.SubItems.Add(emp.Element(elementName_Name).Value);
+                            item1.SubItems.Add(emp.Element(elementName_Surname).Value);
+                            item1.SubItems.Add(emp.Element(elementName_Role).Value);
+                            item1.SubItems.Add(emp.Element(elementName_Salary).Value);
+                            lstEployees.Items.Add(item1);
+                        }
+                        lblTotalEmployee.Text = "Total Employee : " + employees.Count();
+                    }
+                }
+            }
+        }
+
+        //To remove a record from the XML file.
+        public void removeEmployee()
+        {
+            //First we load the element 
+            XElement element = XElement.Load(xml_path);
+            //and we select the specified element with the specified attribute value.
+            var employee = from f in element.Elements(second_element)
+                       where f.Attribute(elementName_ID).Value == txtID.Text
+                       select f;
+            //If there is record with specified ID value
+            if (employee != null)
+            {
+                //Remove the element
+                employee.Remove();
+                //and save the XML document
+                element.Save(xml_path);
+
+                MessageBox.Show("Employee removed successfully.");
+
+                txtID.Text = "";
+                txtName.Text = "";
+                txtSurname.Text = "";
+                txtRole.Text = "";
+                txtSalary.Text = "";
+                //When operation is finished, reload the all records.
+                loadEmployees();
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (txtName.Text != "" && txtSurname.Text != "" && txtRole.Text != "" && txtSalary.Text != "")
+            {
+                //To add new record
+                addNewEmployee();
+            }
+
+        }
+
+        private void lstEployees_Click(object sender, EventArgs e)
+        {
+            //Load the record data to the textboxes
+            txtID.Text = lstEployees.SelectedItems[0].SubItems[0].Text.ToString();
+            txtName.Text = lstEployees.SelectedItems[0].SubItems[1].Text.ToString();
+            txtSurname.Text = lstEployees.SelectedItems[0].SubItems[2].Text.ToString();
+            txtRole.Text = lstEployees.SelectedItems[0].SubItems[3].Text.ToString();
+            txtSalary.Text = lstEployees.SelectedItems[0].SubItems[4].Text.ToString();
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (txtID.Text != "")
+            {
+                //To remove selected record
+                removeEmployee();
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (txtName.Text != "" && txtSurname.Text != "" && txtRole.Text != "" && txtSalary.Text != "")
+            {
+                //To update selected record.
+                updateEmployee();
+            }
+            
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            //To make a live search first clear the listview
+            lstEployees.Items.Clear();
+            //and the check file is exist
+            if (File.Exists(xml_path))
+            {
+                //Load the document
+                XDocument document = XDocument.Load(xml_path);
+                if (document != null)
+                {
+                    //Here we get the records under root element that contains the text in Searchbox.
+                    //Here you should make it all texts toLower, 
+                    //Because C# is capital letter sensitive language. 
+                    IEnumerable<XElement> employees = from f in document.Element(root_Element).Elements(second_element)
+                                                 where f.Element(elementName_Name).Value.ToLower().Contains(txtSearch.Text.ToLower())
+                                                 select f;
+                    //And load the records in the listview if there is record more than one.
+                    if (employees.Count() > 0)
+                    {
+                        foreach (XElement emp in employees) 
+                        {
+                            ListViewItem item1 = new ListViewItem(emp.Attribute(elementName_ID).Value);
+                            item1.SubItems.Add(emp.Element(elementName_Name).Value);
+                            item1.SubItems.Add(emp.Element(elementName_Surname).Value);
+                            item1.SubItems.Add(emp.Element(elementName_Role).Value);
+                            item1.SubItems.Add(emp.Element(elementName_Salary).Value);
+                            lstEployees.Items.Add(item1);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
